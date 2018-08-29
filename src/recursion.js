b5c65548b984ce8624a55f4dfa71ce671782d5eb
Dominic M <dominicccc@gmail.com>
2018-08-28 18:44:21 -0700

    Solved sum

--- src/recursion.js
+++ src/recursion.js
@@ -13,7 +13,8 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
-  return array
+  myArray = array.slice();
+  return myArray.length === 0 ? 0 : myArray.length === 1 ? myArray[0] : myArray.pop() + sum(myArray);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
