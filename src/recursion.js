de9a88205e8252e3db003ab011920834258530ec
Dominic M <dominicccc@gmail.com>
2018-08-31 16:37:35 -0700

    Made some progress on exponent

--- src/recursion.js
+++ src/recursion.js
@@ -42,29 +42,23 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-	// if (n > 0) {
-	  return n === 0 ? 0 : (n - 1) + sumBelow(n-1);
-	// }
-
+  return n === 0 ? 0 : (n - 1) + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
 };
 
-// 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  return exp === 0 ? 1 : base * exponent(base, exp - 1);
 };
 
-// 8. Determine if a number is a power of two.
-// powerOfTwo(1); // true
-// powerOfTwo(16); // true
-// powerOfTwo(10); // false
 var powerOfTwo = function(n) {
 };
 
