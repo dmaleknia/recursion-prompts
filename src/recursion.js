68a9105f6eaea5fbcc759109b2b918c2e61b35b5
Dominic M <dominicccc@gmail.com>
2018-08-31 19:24:26 -0700

    Refactoring

--- src/recursion.js
+++ src/recursion.js
@@ -20,9 +20,7 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
-   return array.reduce((acc, val) => {
-    return acc + (Array.isArray(val) ? arraySum(val) : val)
-  }, 0);
+   return array.reduce((acc, val) => { return acc + (Array.isArray(val) ? arraySum(val) : val); }, 0);
 };
 
 // 4. Check if a number is even.
