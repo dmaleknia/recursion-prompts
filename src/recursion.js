261484fe61368b7f374b6c7d9a10cc3e6bfc5a0d
Dominic M <dominicccc@gmail.com>
2018-09-04 10:27:26 -0700

    Multiply works for positive numbers

--- src/recursion.js
+++ src/recursion.js
@@ -94,6 +94,7 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  return x < y ? multiply(y, x) : y !== 0 ? x + multiply(x, y - 1) : 0;
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
